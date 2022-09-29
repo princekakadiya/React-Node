6c46974a5f4e8daa04caef7116ef058198572c40
princekakadiya <kakadiyaprince902@gmail.com>
2022-09-29 15:41:16 +0530

    bug fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,6 @@ const Header = () => {
             <div className="header-left">
               <button className="mobile-menu-toggler">
                 <span className="sr-only">Toggle mobile menu</span>
-                <i className="icon-bars" />
               </button>
               <a href="/" className="logo ml-5">
                 <img
